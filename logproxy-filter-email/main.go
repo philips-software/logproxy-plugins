e1e152c8a97c45944c3b09a4f8d86567cdaf3f98
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2020-06-08 23:55:30 +0200

    Example filter that triggers an email

---
+++ logproxy-filter-email/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	_ "github.com/cloudfoundry-community/gautocloud/connectors/smtp"
+	"github.com/cloudfoundry-community/gautocloud"
+	"github.com/cloudfoundry-community/gautocloud/connectors/smtp/smtptype"
+	"github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/go-plugin"
+	"github.com/philips-software/go-hsdp-api/logging"
+	"github.com/philips-software/logproxy/shared"
+	_ "github.com/philips-software/gautocloud-connectors/hsdp"
+	"net/smtp"
+	"os"
+	"regexp"
+)
+
+var (
+	triggerEmailPattern = regexp.MustCompile(`Trigger Email`)
+	log = hclog.Default()
+)
+
+type TriggerEmail struct{
+	emailTo string
+	emailFrom string
+	emailSubject string
+	smtpData smtptype.Smtp
+}
+
+
+func (f TriggerEmail) Filter(msg logging.Resource) (logging.Resource, bool, bool, error) {
+	if req := triggerEmailPattern.FindStringSubmatch(msg.LogData.Message); req != nil {
+		// Send email
+		go func() {
+			log.Info("Triggering email!")
+			auth := smtp.PlainAuth("", f.smtpData.User, f.smtpData.Password, f.smtpData.Host)
+			to := []string{f.emailTo}
+			msg := []byte("To: "+f.emailTo+"\r\n" +
+				"Subject: "+f.emailSubject+"\r\n" +
+				"\r\n" +
+				"Here's the email triggered by our plugin\r\n" +
+				"Timestamp: " + msg.LogTime + "\r\n" +
+				"Message: " + msg.LogData.Message + "\r\n")
+			err := smtp.SendMail(fmt.Sprintf("%s:%d", f.smtpData.Host, f.smtpData.Port), auth, f.emailFrom, to, msg)
+			if err != nil {
+				log.Error("error sending email", "error", err.Error())
+			}
+		}()
+	}
+	return msg, false, false, nil
+}
+
+func main() {
+	log.Info("Starting plugin")
+	filter := &TriggerEmail{}
+	err := gautocloud.Inject(&filter.smtpData)
+	if err != nil {
+		log.Error("no SMTP service available", "err", err.Error())
+		return
+	}
+	filter.emailTo = os.Getenv("EMAIL_TO")
+	filter.emailSubject = os.Getenv("EMAIL_SUBJECT")
+	filter.emailFrom = os.Getenv("EMAIL_FROM")
+
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: shared.Handshake,
+		Plugins: map[string]plugin.Plugin{
+			"filter": &shared.FilterGRPCPlugin{Impl: filter},
+		},
+		GRPCServer: plugin.DefaultGRPCServer,
+	})
+}
